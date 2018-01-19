aaa303e929ee955fc7f02040dc461a896d9497c9
edwin rivera <waehake@gmail.com>
2018-01-19 18:21:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 function countdown(callback) {
-  window.setTimeout(callback, 2000);
+  setTimeout(function () {
+    callback();
+  }, 2000);
 }
 
-function createMultiplier(multiplierValue) {
-  return function(value) {
-    return multiplierValue * value;
-  }
+function createMultiplier(multiplyValue) {
+  return function (value) {
+    return multiplyValue * value;
+  };
 }
 
-var doubler = createMultiplier(20);
-var tripler = createMultiplier(20);
-
-function multiplier(one, two) {
-  return one * two;
+function multiplier(a, b) {
+  return a * b;
 }
 
+var doubler = createMultiplier(2);
+var tripler = createMultiplier(3);
 var doublerWithBind = multiplier.bind(null, 2);
 var triplerWithBind = multiplier.bind(null, 3);
