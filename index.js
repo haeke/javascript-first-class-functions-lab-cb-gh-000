f1bed49367584ff0cf363ee9db4c6ad0baac3694
edwin rivera <waehake@gmail.com>
2018-01-19 18:20:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ function createMultiplierBonus(multiplierValue) {
 
 var doubler = createMultiplierBonus(20);
 var tripler = createMultiplierBonus(20);
+
+function multiplier(one, two) {
+  return one * two;
+}
+
+var doublerWithBind = multiplier.bind(null, 2);
+var triplerWithBind = multiplier.bind(null, 3);
