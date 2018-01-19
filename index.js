390addbf0a3ed2a64d1d7f5b63f717d15a36acb2
edwin rivera <waehake@gmail.com>
2018-01-19 18:16:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,5 @@ var tripler = createMultiplier(20);
 function multiplier(a, b) {
   return a * b;
 }
- var doubler = createMultiplier(2);
- var tripler = createMultiplier(3);
- var doublerWithBind = multiplier.bind(null, 2);
- var triplerWithBind = multiplier.bind(null, 3);
+var doublerWithBind = multiplier.bind(null, 2);
+var triplerWithBind = multiplier.bind(null, 3);
