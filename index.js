f43e7f704f26a4d4dc3e433802d4eba7c9cf7a75
edwin rivera <waehake@gmail.com>
2018-01-19 17:42:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
 function countdown(callback) {
   window.setTimeout(callback, 2000);
 }
-
-function createMultiplier(func) {
-  var score = 0;
-  if (func === doubler) {
-    score = doubler(num);
-  } else if (func === tripler) {
-    score = tripler(num)
-  }
-  return function() {
-    console.log("Your score is: " + score);
-  }
-}
-
-function doubler(num) {
-  return num * 2;
-}
-
-function tripler(num) {
-  return num * 3;
-}
-
-var multi = createMultiplier(func);
